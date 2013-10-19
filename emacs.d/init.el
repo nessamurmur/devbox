768c134ea26c3f16c92abcca70bd615222657a95
Nessa Marin <nessa.murmur@gmail.com>
2013-10-18 19:22:28 -0500

    erlang-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,6 +23,11 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-elixir/")
 (require 'elixir-mode)
 
+;; Erlang Mode
+(setq load-path (cons "/usr/local/Cellar/erlang/R16B02/lib/erlang/lib/tools-2.6.12/emacs" load-path))
+(setq erlang-root-dir "/usr/local/Cellar/erlang/R16B02/lib")
+(setq exec-path (cons "/usr/local/Cellar/erlang/R16B02/lib/erlang/bin" exec-path))
+(require 'erlang-start)
 
 ;; YAML Mode
 (require 'yaml-mode)
