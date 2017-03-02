debdf55ade69965fbcca39e863740f7855d151f9
Nessa Marin <nessa.murmur@gmail.com>
2017-03-02 10:52:10 -0800

    Ignore dotfiles for now

--- install.sh
+++ install.sh
@@ -8,22 +8,10 @@ for rcfile in ~/.zprezto/runcoms/*; do ln -s "$rcfile" "~/.${rcfile}"; done; rm 
 
 # These dotfiles
 ssh-keygen -N "" -t rsa -C nessa.murmur@gmail.com
-cat ~/.ssh/id_rsa.pub
-read
-git clone git@github.com:levionessa/dotfiles.git
-cd ~/dotfiles
 
 # Emacs Prelude
 curl -L http://git.io/epre | sh
 
-echo "Copying Files"
-cp zsh/.zshrc ~
-cp zsh/.zpreztorc ~
-cp zsh/.aliases ~
-cp zsh/.exports ~
-cp git/.gitconfig ~
-cp git/.gitignore ~
-mkdir -p ~/.lein
 echo "(setq mode-require-final-newline nil)" >> ~/.emacs.d/personal/whitespace.el
 echo "(add-hook 'before-save-hook 'delete-trailing-whitespace)" >> ~/.emacs.d/personal/whitespace.el
 echo "Done"
