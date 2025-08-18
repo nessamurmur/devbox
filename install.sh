c300e028809ad86fa70b60ccb12ea87ae277e9c9
Nessa Jane Marin <nessajane@nessajane.tech>
2025-08-18 15:29:02 -0700

    Cleanup install script

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 echo "Installing some stuff..."
-sudo apt-get update && sudo apt-get install -y build-essential emacs zsh git curl aspell
+sudo pacman -S emacs
 chsh -s /bin/zsh
 
-echo "Installing Ruby dependencies"
-sudo apt-get install -y gcc-6 autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
-
-# Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-for rcfile in ~/.zprezto/runcoms/*; do ln -s "$rcfile" "~/.${rcfile}"; done; rm ~/.README.md
-
 # Emacs Prelude
-curl -L http://git.io/epre | sh
+curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
 
 cp -r ./zsh/.* ~
 cp -r ./git/.* ~
+
+mkdir -p ~/.emacs.d/personal/
+touch ~/.emacs.d/personal/whitespace.el
 
 echo "(setq mode-require-final-newline 't)" >> ~/.emacs.d/personal/whitespace.el
 echo "(add-hook 'before-save-hook 'delete-trailing-whitespace)" >> ~/.emacs.d/personal/whitespace.el
