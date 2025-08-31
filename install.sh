03aceb1736c72218b80680c2db4e2b6325778407
Nessa Jane Marin <nessajane@nessajane.tech>
2025-08-31 08:57:37 -0700

    Switch to NeoVim & LazyVim

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 chsh -s /usr/bin/zsh
 
 echo "Installing some stuff..."
-sudo pacman -S emacs
+sudo pacman -S neovim
 
 # ruby-build pre-reqs
 sudo pacman -S --needed base-devel rust libffi libyaml openssl
@@ -10,16 +10,5 @@ sudo pacman -S --needed base-devel rust libffi libyaml openssl
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 ~/.rbenv/bin/rbenv init
 
-# Emacs Prelude
-curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
-
-cp -r ./zsh/.* ~
-cp -r ./git/.* ~
-
-mkdir -p ~/.emacs.d/personal/
-touch ~/.emacs.d/personal/whitespace.el
-
-echo "(setq mode-require-final-newline 't)" >> ~/.emacs.d/personal/whitespace.el
-echo "(add-hook 'before-save-hook 'delete-trailing-whitespace)" >> ~/.emacs.d/personal/whitespace.el
-echo "(setq js-indent-level 2)" >> ~/.emacs.d/personal/whitespace.el
-echo "Done"
+# LazyVim
+git clone http://github.com/LazyVim/starter ~/.config/nvim
